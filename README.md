f6985ac5e8da3ef2e9114668426b0d7993703644
Tommy Hodgins <tomhodgins@gmail.com>
2019-09-25 18:57:42 -0400

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
 # hade
-HTML Archive DOM Explorer CLI tool
+
+HTTPArchive DOM Explorer command line tool.
+
+## About
+
+This utility lets you filter and sort data exported from the HTTPArchive crawl of 9,669,416 websites. Enter a search term, an optional matching type, an optional sorting order, and an output format.
+
+> Note: Current data in [data/html-data-aug-2019.json](data/html-data-aug-2019.json) is from August 2019
+
+## Usage
+
+You can use hade with Node or Deno, the options are the same. To search for `<accordion>` tag usage, you can use either `node index.js` or `deno index.js`, followed by the term you wish to search, `accordion`, and any additional options you want to specify. In this case we will search for exact matches to `accordion`, to we can add `-t exact` or `--type exact`:
+
+```bash
+node index.js accordion -t exact
+```
+
+By default the output will look like this:
+
+```bash
+$ node index.js accordion -t exact
+
+<accordion> [mobile] 0.01% (375/5297442)
+<accordion> [desktop] 0% (216/4371974)
+```
+
+This shows us that `<accordion>` is used on `0.01%` of mobile websites observed, and `0%` of desktop websites surveyed. The actual numbers are `375` mobile websites and `216` desktop sites in total.
+
+To get the same information in a different format we can specify `-o json` or `--output json` for JSON output:
+
+```bash
+$ node index.js accordion -t exact -o json
+
+[
+  {
+    "client": "mobile",
+    "element": "accordion",
+    "pages": 375,
+    "totalPages": "5297442",
+    "pct": 0.01
+  },
+  {
+    "client": "desktop",
+    "element": "accordion",
+    "pages": 216,
+    "totalPages": "4371974",
+    "pct": 0
+  }
+]
+```
+
+And to output the results as CSV you can use `-o csv` or `--output csv`:
+
+```bash
+$ node index.js accordion --t exact -o csv
+
+Tag Name, Data set, Percentage, Pages Seen, Pages Crawled
+"accordion","mobile","0.01","375","5297442"
+"accordion","desktop","0","216","4371974"
+```
+
+## Options
+
+### `-m` or `--mobile`
+
+Use this option to exclude mobile data from the output
+
+### `-d` or `--desktop`
+
+Use this option to exclude desktop data from the output.
+
+### `-t` or `--type`
+
+Use this option to specify which type of string matching to use. Available options are:
+
+- `regex`: use search term as a regular expression to match tag name
+- `startsWith`: use search term to match start of tag name to match
+- `endsWith`: use search term to match end of tag name to match
+- `includes`: search term must appear somewhere in tag name to match
+- `exact`: search term must match tag name exactly
+(case sensitive)
+- `loose`: search term must loosely match tag name (case insensitive)
+
+### `-s` or `--sort`
+
+Use this option to specify which sorting option you would like. Available options are:
+
+- `mostUsed`: Show most-used tags at the top of the results, moving toward least-used
+- `leastUsed`: Show least-used tags at the top of the results, moving toward most-used
+- `alphabetical`: Show tags in alphabetical order
+- `resverse`: Show tags in reverse alphabetical order
+- `mobile`: Show mobile tags first
+- `desktop`: Show desktop tags first
+
+### `-o` or `--output`
+
+You can use this option to specify the format of the output. By default it will display information in a format for reading on the command-line. Available options are:
+
+- `json` for JSON-formatted data
+- `csv` for Comma Separated Value formatting for use as a spreadsheet
+
+### `-h` or `--help`
+
+Display help text in the console.
+
+## Examples
+
+To display stats for `<body>` tag usage:
+
+```bash
+$ node hade.js body -t exact
+```
+
+To find all tag names starting with `modal-`
+
+```bash
+$ deno hade.js modal- -t startsWith
+```
+
+To find all tag names ending with `-modal` sorted alphabetically
+
+```bash
+$ node hade.js -modal -t endsWith -s alphabetical
+```
+
+To display JSON output
+
+```bash
+$ deno hade.js noscript -t exact -o json
+```
+
+To display all HTML tags in order of use
+
+```bash
+$ node hade.js "^(a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|bgsound|big|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|command|content|data|datalist|dd|del|details|dfn|dir|dialog|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|image|img|input|ins|kbd|keygen|label|layer|legend|li|link|listing|main|map|mark|marquee|menu|meta|meter|nav|nobr|noembed|noframes|nolayer|object|ol|optgroup|option|output|shadow|p|param|picture|plaintext|portal|pre|progress|q|rb|rp|rt|rtc|ruby|s|samp|script|section|select|slot|small|source|span|strike|strong|style|sub|summary|sup|table|tbody|td|template|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr|xmp|noscript)\\$"
+```
---
+++ index.js
@@ -0,0 +1,211 @@
+let data
+let args
+let options = {
+  term: '',          // always the first argument
+  device: {
+    mobile: true,    // -m or --mobile to exclude
+    desktop: true    // -d or --desktop to exclude
+  },
+  type: 'regex',     // -t or --type {regex | startsWith | endsWith | contains | matches}
+  sort: 'mostUsed',  // -s or --sort {mostUsed | leastUsed | alphabetical}
+  output: 'cli',     // -o or --output {cli | json}
+  helpMode: false    // -h or --help to output help text
+}
+
+const filtering = {
+  regex: string =>  data.filter(({element}) => {
+    let [pattern, flags] = string.split('/')
+    return element.match(new RegExp(pattern, flags))
+  }),
+  startsWith: string => data.filter(({element}) => element.startsWith(string)),
+  endsWith: string => data.filter(({element}) => element.endsWith(string)),
+  includes: string => data.filter(({element}) => element.includes(string)),
+  exact: string => data.filter(({element}) => element === string),
+  loose: string => data.filter(({element}) =>
+    element.toLowerCase().trim() === string.toLowerCase().trim()  
+  )
+}
+
+const sorting = {
+  mostUsed: obj => obj.sort((a, b) => b.pct - a.pct),
+  leastUsed: obj => obj.sort((a, b) => a.pct - b.pct),
+  alphabetical: obj => obj.sort((a, b) => a.element[0].charCodeAt(0) - b.element[0].charCodeAt(0)),
+  reverse: obj => obj.sort((a, b) => b.element[0].charCodeAt(0) - a.element[0].charCodeAt(0)),
+  mobile: obj => obj.sort((a, b) => b.client[0].charCodeAt(0) - a.client[0].charCodeAt(0)),
+  desktop: obj => obj.sort((a, b) => a.client[0].charCodeAt(0) - b.client[0].charCodeAt(0))
+}
+
+const formatting = {
+  cli: arr => arr
+    .map(obj => `<${obj.element}> [${obj.client}] ${obj.pct}% (${obj.pages}/${obj.totalPages})`)
+    .join('\n'),
+  csv: arr => 'Tag Name, Data set, Percentage, Pages Seen, Pages Crawled\n'
+    + arr
+      .map(({client, element, pages, totalPages, pct}) => 
+        [element, client, pct, pages, totalPages]
+          .map(cell => `"${String(cell).replace(/"/g, '""')}"`)
+          .join(',')
+      )
+      .join('\n'),
+  json: arr => JSON.stringify(arr, null, 2)
+}
+
+// Deno CLI support
+if (typeof Deno !== 'undefined') {
+  args = Deno.args.slice(1)
+  data = JSON.parse(
+    new TextDecoder('utf-8').decode(
+      Deno.readFileSync('./data/html-data-aug-2019.json')
+    )
+  )
+}
+
+// Node CLI support
+if (typeof process !== 'undefined') {
+  args = process.argv.slice(2)
+  require = require || function() {}
+  data = require('./data/html-data-aug-2019.json')
+}
+
+let desktopTotal = Number(data.find(obj => obj.client === 'desktop').totalPages)
+let mobileTotal = Number(data.find(obj => obj.client === 'mobile').totalPages)
+
+// Argument parsing
+args.forEach((arg, index, list) => {
+
+  // First argument is always our search term
+  if (index === 0) {
+    options.term = arg
+  }
+
+  // -m or --mobile to include mobile data in output
+  if (
+    ['-m', '--mobile'].some(term => term === arg)
+  ) {
+    options.device.mobile = false
+  }
+
+  // -d or --desktop to include desktop data in output
+  if (
+    ['-d', '--desktop'].some(term => term === arg)
+  ) {
+    options.device.desktop = false
+  }
+
+  // -t or --type to specify type of filter to use
+  if (
+    ['-t', '--type'].some(term => term === arg)
+    && list[index + 1]
+    && typeof list[index + 1] === 'string'
+    && Object.keys(filtering).some(term => term === list[index + 1])
+  ) {
+    options.type = list[index + 1]
+  }
+
+  // -s or --sort to specify type of sorting to use
+  if (
+    ['-s', '--sort'].some(term => term === arg)
+    && list[index + 1]
+    && typeof list[index + 1] === 'string'
+    && Object.keys(sorting).some(term => term === list[index + 1])
+  ) {
+    options.sort = list[index + 1]
+  }
+
+  // -o or --output to choose output format (JSON or CLI output)
+  if (
+    ['-o', '--output'].some(term => term === arg)
+    && list[index + 1]
+    && typeof list[index + 1] === 'string'
+    && Object.keys(formatting).some(term => term === list[index + 1])
+  ) {
+    options.output = list[index + 1]
+  }
+
+  // -h or --help to output help text, or no args given
+  if (['-h', '--help'].some(term => term === arg)) {
+    options.helpMode = true
+  }
+})
+
+// If no arguments given, enable help mode
+if (args.length === 0) {
+  options.helpMode = true
+}
+
+// Remove the default object from the data
+data = data.filter(({client}) => client !== 'client')
+
+// Optionally remove mobile data
+if (options.device.mobile === false) {
+  data = data.filter(({client}) => client !== 'mobile')
+}
+
+// Optionally remove desktop data
+if (options.device.desktop === false) {
+  data = data.filter(({client}) => client !== 'desktop')
+}
+
+// Apply search term, filter, and sort to data
+data = formatting[options.output](
+  sorting[options.sort](
+    filtering[options.type](
+      options.term
+    )
+  )
+)
+
+const helpMessage = () => `
+HTTPArchive DOM Explorer
+
+About:
+
+This utility lets you filter and sort data exported from the HTTPArchive crawl of ${(desktopTotal + mobileTotal).toLocaleString()} websites. Enter a search term, an optional matching type, an optional sorting order, and an output format.
+
+Options:
+
+  -m, --mobile     exclude mobile data from output
+  -d, --desktop    exclude desktop data from output
+  -t, --type       specify which type of term matching to use
+  -s, --sort       specify the ordering of the results
+  -o, --output     declare the formatting of the output
+  -h, --help       display this help message
+
+Matching types available:
+${Object.keys(filtering).map(plugin => `\n  - ${plugin}`).join('')}
+
+Sorting orders available:
+${Object.keys(sorting).map(plugin => `\n  - ${plugin}`).join('')}
+
+Output formats available:
+${Object.keys(formatting).map(plugin => `\n  - ${plugin}`).join('')}
+
+Usage:
+
+  To display stats for <body> tag usage:
+
+    $ node hade.js body -t exact
+
+  To find all tag names starting with 'modal-'
+
+    $ deno hade.js modal- -t startsWith
+
+  To find all tag names ending with '-modal' sorted alphabetically
+
+    $ node hade.js -modal -t endsWith -s alphabetical
+
+  To display JSON output
+
+    $ deno hade.js noscript -t exact -o json
+
+`
+
+// Output formatted result to console if not empty and no help text displayed
+if (
+  options.helpMode === false
+  && data.length
+) {
+  console.log(data)
+} else if (options.helpMode === true) {
+  console.log(helpMessage())
+}
